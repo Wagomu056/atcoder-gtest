c859a13559785e1990ef74fe2c73d66f3d456a28
Takuya Azumaguchi <catchballer@gmail.com>
2022-02-05 15:42:26 +0900

    test_macro: ファイル読み込みマクロ追加

--- tests/test_macro.h
+++ tests/test_macro.h
@@ -1,2 +1,3 @@
 #define PREPARE(_STR_) std::stringbuf s(_STR_); std::cin.rdbuf(&s); testing::internal::CaptureStdout();
+#define PREPARE_F(_FILE_) std::ifstream ifs(_FILE_); if (!ifs) { std::cerr << "open fail." << std::endl; ASSERT_TRUE(false); } std::cin.rdbuf(ifs.rdbuf()); testing::internal::CaptureStdout();
 #define CHECK(_STR_) const std::string outstr = testing::internal::GetCapturedStdout(); EXPECT_EQ(outstr, _STR_ "\n");
